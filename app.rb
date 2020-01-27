9ba0ca406d66ab8981e77511217cacccf0078357
James Loomos <jtloomos@gmail.com>
2020-01-27 14:41:02 -0300

    Boilerplate sinatra app

--- app.rb
+++ app.rb
@@ -1,8 +1,7 @@
 require "sinatra"
-require 'sinatra/reloader' if development?
-require 'pry-byebug'
-require 'better_errors'
-
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
